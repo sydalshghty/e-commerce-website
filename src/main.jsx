4d14dc45ca29e929df4badb8eab8ef5d92094e7c
sydalshghty <168644290+sydalshghty@users.noreply.github.com>
2025-04-19 15:43:08 +0200

    update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='e-commerce-website'>
         <Provider store={store}>
             <App />
         </Provider>
